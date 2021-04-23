d6b8ca2ce6a5c0aedee4c445e1a6613bba4c216d
Connor Slade <turtleslade@gmail.com>
2021-04-23 19:59:55 -0400

    ℹ Init

---
+++ src/index.py
@@ -0,0 +1,105 @@
+# RandomSong
+# Replace words of a song with its synonyms
+# By Connor Slade :P
+
+from datetime import datetime
+import random
+import json
+
+############ VARS ############
+wordFile = 'data/words.json'
+songFile = 'data/song.txt'
+outFile = 'data/out.txt'
+
+RandomIndex = True
+
+DEBUG = True
+COLOR = True
+
+ColorCodes = {'black': '30', 'red': '31', 'yellow': '33', 'green': '32', 'blue': '34',
+              'cyan': '36', 'magenta': '35', 'white': '37', 'gray': '90', 'reset': '0'}
+
+######### FUNCTIONS #########
+
+
+def colored(text, color):
+    if not COLOR:
+        return text
+    return '\033[' + ColorCodes[str(color).lower()] + 'm' + str(text) + "\033[0m"
+
+
+def DebugPrint(Category, Text, Color):
+    if not DEBUG:
+        return
+    print(colored('['+datetime.now().strftime("%H:%M:%S")+'] ', 'yellow') +
+          colored('['+Category+'] ', 'magenta')+colored(Text, Color))
+
+
+def loadWordData(file):
+    DebugPrint('WordFile', 'Loading File', 'cyan')
+    try:
+        data = open(file, 'r', encoding='utf-8').read()
+    except:
+        DebugPrint('WordFile', 'Error Reading File', 'red')
+        return None
+    DebugPrint('WordFile', 'Success', 'green')
+    DebugPrint('WordFile', 'Starting Parse', 'cyan')
+    try:
+        jsonLoaded = json.loads(data)
+    except:
+        DebugPrint('WordFile', 'Error : /', 'red')
+        return None
+    DebugPrint('WordFile', 'Complete', 'green')
+    return jsonLoaded
+
+
+def getWordForWord(data, word, RandomIndex):
+    words = data[word]
+    index = random.randint(0, len(words)-1)
+    rng = words[RandomIndex if RandomIndex != True else index]
+    return rng
+
+
+def forEachInSong(songFile, wordData, RandomIndex):
+    DebugPrint('SongFile', 'Loading File', 'cyan')
+    try:
+        data = open(songFile, 'r', encoding='utf-8').read()
+    except:
+        DebugPrint('SongFile', 'Error Reading File', 'red')
+        return None
+    DebugPrint('SongFile', 'Success', 'green')
+    DebugPrint('SongFile', 'Genarateing Song', 'cyan')
+    working = ''
+    words = data.split(' ')
+    for word in words:
+        try:
+            working += getWordForWord(wordData, word, RandomIndex)
+        except:
+            working += word
+        working += ' '
+    DebugPrint('SongFile', 'Done :P', 'green')
+    return working
+
+
+def saveNewSong(outFile, data):
+    DebugPrint(
+        'Output', f'Saveing File {colored("["+outFile+"]", "blue")}', 'cyan')
+    file = open(outFile, 'w', encoding='utf-8')
+    file.write(data)
+    file.close()
+    DebugPrint('Output', 'Saveing Complete', 'green')
+
+####### MAIN FUNCTION #######
+
+
+def main():
+    data = loadWordData(wordFile)
+    newSong = forEachInSong(songFile, data, RandomIndex)
+    saveNewSong(outFile, newSong)
+    #print(getWordForWord(data, 'happy'))
+
+
+if __name__ == "__main__":
+    DebugPrint('Main', 'Starting...', 'green')
+    main()
+    DebugPrint('Main', 'Exiting...', 'red')
